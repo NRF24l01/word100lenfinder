7281d6a2326b36bd8ca20b0c5f5f0a8dedf4aa9f
stenli <s.nevrovsky@ya.ru>
2023-11-07 08:28:57 +0300

    add global data prints

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from class_data import Word
 
 with open("words.txt", "r", encoding="utf-8") as f:
     words = f.read().split("\n")
+    wk = 0
+    print(f"Всего слов {len(words)}")
     for i in words:
         w = Word(i)
         wl = w.get_word_sum()
         if wl == 100:
-            print("Виха!")
             print(f"Слово: {i}, сумма: {wl}")
+            wk+=1
+    print(f"Найдено слов: {wk}")
